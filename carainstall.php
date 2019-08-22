4eba70c126524c9e6c02546943a95c27ada6394d
garinscater <53307606+garinscater@users.noreply.github.com>
2019-08-22 20:38:49 +0700

    carainstall.php

---
+++ carainstall.php
@@ -0,0 +1,20 @@
+_   _    _    ____ _  _______ ____
+| | | |  / \  / ___| |/ / ____|  _ \
+| |_| | / _ \| |   | ' /|  _| | |_) |
+|  _  |/ ___ \ |___| . \| |___|  _ <
+|_| |_/_/   \_\____|_|\_\_____|_| \_\
+
++-------------------------------+
+| author:Mr.G4R1N               |
+| FB :G4R1N SC4T3R              |
+| YT:Hacker Elite ID            |
+| Github:github.com/garinscater |
++-------------------------------+
+Pkg install nano
+Cd .. 
+Cd usr/etc
+Nano bash.bashrc 
+Ganti PS1='nama' 
+
+Untuk tools lainnya silahkan hubungi wa admin
+No wa :0895397189863 
