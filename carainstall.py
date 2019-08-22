6aba89677beed0b8350af56a50082ffee0566082
garinscater <53307606+garinscater@users.noreply.github.com>
2019-08-22 20:02:28 +0700

    carainstall.py

---
+++ carainstall.py
@@ -0,0 +1,12 @@
+
+
+echo '+-------------------------------+' | lolcat 
+echo '| author:Mr.G4R1N               |' | lolcat
+echo '| YT:Hacker Elite ID            |' | lolcat
+echo '| Github:github.com/garinscater |' | lolcat
+echo '+-------------------------------+' | lolcat
+echo 'Pkg install nano' | lolcat
+echo 'Cd ..' | lolcat
+echo 'Cd usr/etc' | lolcat
+Nano bash.bashrc | lolcat
+Ganti PS1='nama' | lolcat
