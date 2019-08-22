f2b884dd6b928f80dfaa3336e67571e4e028dfc0
garinscater <53307606+garinscater@users.noreply.github.com>
2019-08-22 20:13:52 +0700

    Update carainstall.php

---
+++ carainstall.php
@@ -0,0 +1,13 @@
++-------------------------------+' 
+| author:Mr.G4R1N               |' 
+| YT:Hacker Elite ID            |' 
+| Github:github.com/garinscater |' 
++-------------------------------+' 
+Pkg install nano' | lolcat
+Cd .. 
+Cd usr/etc
+Nano bash.bashrc 
+Ganti PS1='nama' 
+
+Untuk tools lainnya silahkan hubungi wa admin
+No wa :0895397189863 
